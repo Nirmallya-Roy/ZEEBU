92100cbcf47cff8f896f07af3a7aecd4d0ea6103
Nirmallya Roy <nirmallyaroyroy@gmail.com>
2024-10-05 00:48:13 -0700

    Pushing 1st page without meteor

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 var tl = gsap.timeline()
-tl.from("img,i",{
+tl.from(".planet",{
+    y:200,
+    duration:1
+})
+tl.from(".glow",{
+    y:200,
+    
+    opacity:0
+})
+tl.from(".i2",{
+    y: -100,
+    
+    duration: 1,
+    ease: 'elastic.out(1, 0.3)',
+},"-=1")
+tl.from("h1,p",{
+    scale: 10,
+    opacity:0,
+    duration:1,
+    alignItems:'end'
+})
+tl.from(".i1,i",{
     rotate:360,
     opacity: 0,
     duration: 1,
-    delay:0.5
+    
 })
 tl.from("h2,button",{
     
     opacity: 0,
     duration: 2,
-    delay:0.5
+    
 },"-=1")
 tl.from("h4",{
     y: -20,
@@ -17,3 +38,4 @@ tl.from("h4",{
     duration: 0.5,
     stagger:0.3
 },"-=1")
+
